8ebd26c1eefbe68fd6679e2e38b7ca2269be15ff
Gr0sCrane <Mouadchd05@gmail.com>
2025-08-02 22:15:05 +0100

    inital commit with Cmake and raylib linking

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <raylib.h>
 
-int main(){
-    std::cout<<"Hello"<<"\n";
+int main() {
+    
+    InitWindow(800, 600, "GravityZone-alphaBuild");
+
+    // Définit le nombre de frames par seconde
+    SetTargetFPS(60);
+
+    // Boucle principale
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        DrawText("Hello test !", 190, 280, 20, WHITE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
